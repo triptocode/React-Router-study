25bcbc7647f1ef17df5d3bce2aa838f7608712b8
triptocode <3580rio@gmail.com>
2023-04-02 07:46:19 +0900

    리액트 라우터

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1> About  </h1>
+      <p> 이 사이트는 리액트 라우터를  실습 해보는 예제 프로젝트를 다룹니다</p>
+    </div>
+  );
+};
+
+export default About;
